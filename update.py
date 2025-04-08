013a0a65bdef356a26948b96bdadb6049313f7b9
jugaddor <166486324+jugaddor@users.noreply.github.com>
2025-04-08 09:26:14 +0500

    update.py

--- update.py
+++ update.py
@@ -4,10 +4,10 @@
     sudo apt-get update
     sudo apt-get install -y ffmpeg
     
-    # Используем ffmpeg для проверки ссылок
+    # Используем ffprobe для проверки ссылок
     while IFS= read -r line; do
-      if [[ $line == http* ]]; then
-        if ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$line" &> /dev/null; then
+      if [[ "$line" == http* ]]; then
+        if ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$line" > /dev/null 2>&1; then
           echo "$line"
         fi
       else
